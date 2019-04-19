9c45610192029b1c27f7852c7a84e31916c9b2c8
Taylor Stefanovicz <trstefanovicz@gmail.com>
2019-04-18 22:51:32 -0400

    Update title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-/** Test heroku*/
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mls';
+  title = 'Metropolitan Legal Solutions';
 }
