2b886b1bf61da96374daf960c90cf0eed3db647f
Taylor Stefanovicz <trstefanovicz@gmail.com>
2019-04-21 01:42:34 -0400

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { AuthService } from './auth.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ import { AuthService } from './auth.service';
 export class AppComponent {
   title = 'Metropolitan Legal Solutions';
 
-  constructor(private authService: AuthService, public router: Router) {
+  constructor(private authService: AuthService, public router: Router,private titleService: Title ) {
     this.router.events.subscribe((ev) => {
       if (ev instanceof NavigationEnd) {
         console.log(authService.protected);
       }
     });
+    this.titleService.setTitle('Metropolitan Legal Solutions');
   }
 }
