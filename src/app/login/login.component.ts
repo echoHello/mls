4e805030f4845f954c5ab97eab0717d1a9eb1ee8
Taylor Stefanovicz <trstefanovicz@gmail.com>
2019-04-21 01:15:02 -0400

    add home page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { User } from '../user';
 import { AuthService } from '../auth.service';
 
 @Component({
